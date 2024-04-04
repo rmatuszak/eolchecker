6536fefbc4a2d5a32e904df2e6cdf863d4b40415
rmatusza <rafal.matuszak@intel.com>
2024-04-04 07:17:43 +0000

    Fix results printing

--- pre_commit_hooks/eol_checker.py
+++ pre_commit_hooks/eol_checker.py
@@ -27,11 +27,11 @@ def check_eol(filename: str, expected_eol: str) -> bool:
 
     if not_expected > 0:
         print(f"{filename} didn't pass check! Lines to fix:")
-        print(lines_to_fix)
-        return True
+        # print(lines_to_fix)
+        return True, lines_to_fix
     else:
-        print("All checks passed!")
-        return False,
+        # print("All checks passed!")
+        return False, lines_to_fix
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
@@ -46,9 +46,14 @@ def main(argv: Sequence[str] | None = None) -> int:
 
     hook_ret = 0
     for filename in args.filenames:
-        res = check_eol(filename, args.eol)
+        res, lines = check_eol(filename, args.eol)
         if res:
             hook_ret = -1
+            print(f"Not all files passed the check! Please refer to below lines in {filename}:")
+            for line in lines:
+                print(line)
+        else:
+            print("All files passed!")
     return hook_ret
 
 if __name__ == "__main__":
