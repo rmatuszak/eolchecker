1957f09e20f4d1de13416f93878ad2365f9dbf9f
rmatusza <rafal.matuszak@intel.com>
2024-04-08 19:41:36 +0000

    Cleanup

--- pre_commit_hooks/eol_checker.py
+++ pre_commit_hooks/eol_checker.py
@@ -25,29 +25,7 @@ def parse_eols(files: str, expected_eol: str) -> None:
             files_to_fix.append(e.split('\t')[1])
     return files_to_fix
 
-# def check_eols(filename: str, expected_eol: str) -> bool:
-#     with open(filename, 'rb') as f:
-#         contents = f.read()
-
-#     not_expected=0
-#     lines_to_fix=[]
-#     if expected_eol in ACCEPTED_EOLS.keys():
-#         for idx,line in enumerate(contents.splitlines(True)):
-#             if not line.endswith(ACCEPTED_EOLS[expected_eol]):
-#                 print(line)
-#                 not_expected += 1
-#                 lines_to_fix.append(f"{idx}: {line}")
-#     else:
-#         raise "Error! Presented EOL is not in the list of available to parse EOLs! Exiting."
-
-#     if not_expected > 0:
-#         return True, lines_to_fix
-#     else:
-#         return False, lines_to_fix
-
 def main(argv: Sequence[str] | None = None) -> int:
-
-    # argument parsing part
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-e', '--eol',
@@ -58,10 +36,8 @@ def main(argv: Sequence[str] | None = None) -> int:
     parser.add_argument('filenames', nargs='*', help='Filenames to check')
     args = parser.parse_args(argv)
 
-    # get eols
     ftf = parse_eols(args.filenames, args.eol)
 
-    # all results printing should happen here, after main for loop
     hook_ret=0
 
     if len(ftf) > 0:
