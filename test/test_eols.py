8034df3d4d60566b8feb748289ecc5cb959d8b20
rmatusza <rafal.matuszak@intel.com>
2024-04-09 10:42:42 +0000

    Add manual tests

---
+++ test/test_eols.py
@@ -0,0 +1,36 @@
+def generate_mixed_eol_file() -> None:
+    with open("mixed_eols", "wb+") as file:
+        for i in range(1,10):
+            if i % 2 != 0:
+                print("LF line ending!")
+                file.write(b"LF line ending!\n")
+            else:
+                print("CRLF line ending!")
+                file.write(b"CRLF line ending!\r\n")
+        file.close()
+
+def generate_exact_eols(eol: str) -> None:
+    with open(f"{eol}_eols", "wb+") as file:
+        for _ in range(1,10):
+            print(f"{eol} line ending!")
+            file.write(b"Line to save with LF!\n") if eol == "lf" else file.write(b"Line to save with CRLF!\r\n")
+        file.close()
+
+def view_eol_file(filename: str) -> None:
+    with open(filename,"rb+") as file:
+        for l in file.readlines():
+            print(l)
+
+eol="crlf"
+
+print("MIXED EOL GENERATION")
+generate_mixed_eol_file()
+
+print("EXACT EOL GENERATION")
+generate_exact_eols(f"{eol}")
+
+print("TESTING MIXED")
+view_eol_file('mixed_eols')
+
+print("TESTING EXACT")
+view_eol_file(f"{eol}_eols")
